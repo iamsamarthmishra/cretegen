6526ea092443bd1ed9326bfa376d28031f63d62a
Samarth277 <91658673+Samarth277@users.noreply.github.com>
2023-02-04 22:57:10 +0530

    not for you

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -2,7 +2,7 @@
 (function () {
   "use strict";
 
-  let forms = document.querySelectorAll('.php-email-form');
+  let forms = document.querySelectorAll('https://formspree.io/f/meqweblo');
 
   forms.forEach( function(e) {
     e.addEventListener('submit', function(event) {
